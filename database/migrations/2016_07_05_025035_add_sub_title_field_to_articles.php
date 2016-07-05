09ca723f6d920ff9f2087f76fac8a4a41fae2b08
micsbits <willow6786@gmail.com>
2016-07-04 22:59:19 -0400

    added subtitle field to articles for extra text under a heading

---
+++ database/migrations/2016_07_05_025035_add_sub_title_field_to_articles.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSubTitleFieldToArticles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->string('subtitle', 50)->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropColumn('subtitle');
+        });
+    }
+}
